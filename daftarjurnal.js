f0e240dccf3b14b340e8b928e62f7ea7d2358733
Sigit Purwadi <sgt2426@gmail.com>
2016-04-04 18:20:21 +0700

    update

--- daftarjurnal.js
+++ daftarjurnal.js
@@ -5,7 +5,6 @@ var options = {
 
 var values = [{
     nama: 'Jonny Strömberg',
-	tanggal: 
     tautan: 1986
   },
   {
@@ -14,7 +13,6 @@ var values = [{
   },
   {
     nama: 'LIPI',
-	tanggal: 3 April 2016
     tautan: http://www.jurnal.lipi.go.id/
 }];
 
